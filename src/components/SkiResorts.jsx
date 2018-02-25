a947d57103214b51b7c822241145c1bcb08486e4
Ewa Leszczuk-Czubkowska <ewales93@gmail.com>
2018-02-25 15:12:20 +0100

    add skireport list

---
+++ src/components/SkiResorts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './SkiResorts.css';
+
+class SkiResorts extends Component {
+  render() {
+    return (
+      <div className="SkiResortsWrapper">
+      </div>
+    );
+  }
+}
+
+export default SkiResorts;
